f715dad24edbcab16529d9cddcb0a5791ba677f3
peaton373 <peaton373@gmail.com>
2020-04-20 15:10:11 -0500

    created app.py file, added pseudocode and created class for word, will update further

---
+++ app.py
@@ -0,0 +1,44 @@
+# CLASSES HERE #
+
+class Word():
+  # avoid referencing the game object inside the Word class if possible
+  def __init__(self, word): 
+    # word bank here?
+    # choose random word from word bank
+    # use it to set up a data structure to track correctly guessed letters
+      # maybe an array of objects? // if you use a Letter class, those objects could be instances of that class
+    self.identity = "user"
+    self.word = word
+print(word)
+    # update/display the word with letters for correctly guessed letters and underscores for unguessed letters
+    # maybe have spaces between the letters/underscores for readability?
+  
+ 
+    # see if it's in word, 
+    # change data structure appropriately
+    # call printWord() on this instance
+
+
+
+
+# GAME OBJECT HERE #
+
+    # resets guessedLetters
+    # resets guessesRemaining
+    # call getWord?
+
+    # instantiate your word class and store it in this object?
+    # prompt (using DOM) user to start guessing, or call another method that does this
+
+    # update/display guessed letters
+    # update/display guesses remaining
+        # perhaps?  
+
+  
+  # what else?
+  # some kind of functionality to have game end in loss if they run out of guesses?
+  # and to have game end in a win if they guess all the letters correctly?
+
+
+
+#  LISTENERS HERE #
